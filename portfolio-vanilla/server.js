2addf77bee0a4a3a86b67056f49b83ff3d262911
Felix Sebastian <hi@felixsebastian.dev>
2022-02-06 07:30:14 +0000

    add server.js

---
+++ portfolio-vanilla/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 7500
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect('index.html')
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
